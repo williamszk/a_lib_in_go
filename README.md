99777778f3b386e4e79e7354f217ed4255529966
williamszk <wllmszk@gmail.com>
2023-04-12 23:52:28 -0300

    just adding tag to git

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # a_lib_in_go
 
+https://www.youtube.com/watch?v=KNHEXOoV-H4&t=64s&ab_channel=BrianMorrison
+
 Just a lib in Go for me to test downloading it.
 
 We build some simple functions in this lib.
--- notes.sh
+++ notes.sh
@@ -1,8 +1,14 @@
 
 go mod init github.com/williamszk/a_lib_in_go 
 
+go mod init github.com/williamszk/a_lib_in_go
+
 go test
 
 go run main.go
 
 go build
+
+# we need to tag
+git tag "v1.0.0"
+git push --tags
